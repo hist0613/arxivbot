9e4effc8302227d9174517a84b0a4143516195ce
hist0613 <hist0134@naver.com>
2024-11-20 00:44:37 +0900

    Update prompt

--- prompts.py
+++ prompts.py
@@ -1,6 +1,6 @@
-SYSTEM_PROMPT_SUMMARIZATION = """Please analyze and summarize the arxiv paper into an **Korean** AI newsletter. Feel free to include some technical keywords in English itself. You can write side-by-side the original English words in parenthesis if the words are not familiar or not frequently used in Korean. Please answer in JSON format where **keys are in English**. 
+SYSTEM_PROMPT_SUMMARIZATION = """Please analyze and summarize the arxiv paper into an **Korean** AI newsletter with 3-4 sentences for each section. Feel free to include some technical keywords in English itself. You can write side-by-side the original English words in parenthesis if the words are not familiar or not frequently used in Korean. Please answer in JSON format where **keys are in English**. 
 
-arxiv 논문을 분석하고 요약해주세요. 기술적인 키워드는 영어로 적어도 좋습니다. 한국어로 자연스럽지 않거나 자주 사용되지 않는 단어는 영어로 괄호 안에 적어주세요.
+arxiv 논문을 분석하고 요약해주세요. 각 섹션별로 3-4문장으로 작성해주세요. 기술적인 키워드는 영어로 적어도 좋습니다. 한국어로 자연스럽지 않거나 자주 사용되지 않는 단어는 영어로 괄호 안에 적어주세요.
 
 Consider the following format and components for the summary (but don't include all the keys if not applicable):
 [
@@ -11,6 +11,7 @@ Consider the following format and components for the summary (but don't include 
 """
 
 from pydantic import BaseModel
+
 
 class GptSummarizationResponse(BaseModel):
     whats_new: str
